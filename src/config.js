bea08cfd3d7727b5a93bb029a144947990be4032
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-06 19:50:51 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "wlxchange-user-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://jzkcx6pzz9.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_wxm4Jft6X",
+      APP_CLIENT_ID: "6fk1bb8q7bmjqo3quu2c1oecgi",
+      IDENTITY_POOL_ID: "us-east-2:aa523442-e976-42c9-b9be-028356069091"
+    }
+  };
