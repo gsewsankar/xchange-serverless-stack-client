8b4b7fe197589bcd918ce15cd6787433533a1509
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-07 14:57:00 -0400

    Adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,41 @@
-export function onError(error) {
-    let message = error.toString();
-  
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
-    }
-  
-    alert(message);
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
   }
+
+  Sentry.init({dsn: "https://c7688a0c653a46eabab81f5c1e8b28e4@o389319.ingest.sentry.io/5227360"});
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
+export function onError(error) {
+  let errorInfo = {};
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
+
+  logError(error, errorInfo);
+
+  alert(message);
+}
