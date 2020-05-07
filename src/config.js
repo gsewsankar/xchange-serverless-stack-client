1863b480608b1ca294e8d00d3119d5964f3cfd79
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-07 11:52:42 -0400

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
+const dev = {
+  STRIPE_KEY: "pk_test_wuCspdEm0g5jRark4XCXmdeI00GRCvDXgj",
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "xchange-app-2-api-dev-serverlessdeploymentbucket-13il4q0tzevda"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ai9lz6bbt5.execute-api.us-east-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_9to36kKYo",
+    APP_CLIENT_ID: "4oi0buak2siitgjuettuutp7dk",
+    IDENTITY_POOL_ID: "us-east-2:c5995d96-785c-4f7f-ac49-3d837d7ffb38"
+  }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_wuCspdEm0g5jRark4XCXmdeI00GRCvDXgj",
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "xchange-app-2-api-prod-serverlessdeploymentbucket-1bf9xo26778fs"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://0xt7970mi0.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_uJtsG7Q4M",
+    APP_CLIENT_ID: "6o9kkcjl8dthadv2fqltt85v0g",
+    IDENTITY_POOL_ID: "us-east-2:efb3bc6b-a079-4286-970b-0810af9e0e14"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
 export default {
+  // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "xchange-app-api-dev-serverlessdeploymentbucket-670qczry8w94"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "https://jzkcx6pzz9.execute-api.us-east-2.amazonaws.com/dev"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_wxm4Jft6X",
-      APP_CLIENT_ID: "6fk1bb8q7bmjqo3quu2c1oecgi",
-      IDENTITY_POOL_ID: "us-east-2:aa523442-e976-42c9-b9be-028356069091"
-    },
-    STRIPE_KEY: "pk_test_wuCspdEm0g5jRark4XCXmdeI00GRCvDXgj",
-  };
+  ...config
+};
