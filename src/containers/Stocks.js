8e9981e4d2327e241fd330467b38f1243def67bd
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-23 20:30:41 -0400

    removed graphing garbage

--- src/containers/Stocks.js
+++ src/containers/Stocks.js
@@ -1,7 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useParams} from "react-router-dom";
 import { onError } from "../libs/errorLib";
-import {Line} from 'react-chartjs-2';
 import "./Stocks.css";
 
 import StockSearchBar from "../components/StockSearchBar";
@@ -12,12 +11,22 @@ export default function Stocks() {
     const { id } = useParams();
     const [stock, setStock] = useState("");
     const [price, setPrice] = useState("");
+    const labels = [];
+    const priceData= [];
     
   useEffect(() => {
     function loadStock() {
       alpha.data.quote(id, "compact", "1min").then(data => {
         setStock(data['Global Quote']['01. symbol']);
         setPrice(data['Global Quote']['05. price']);
+      });
+
+      alpha.data.intraday(id, "compact", "1min").then(data => {
+        console.log(data);
+        for(var key in data["Time Series (1min)"]){
+          labels.push(key);
+          priceData.push(data["Time Series (1min)"][key]["1. open"]);
+        }
       });
     }
 
@@ -28,45 +37,17 @@ export default function Stocks() {
         onError(e);
       }
     }
-
     onLoad();
   }, [id]);
 
-  const chartdata = {
-    labels: ['January', 'February', 'March',
-             'April', 'May'],
-    datasets: [
-      {
-        label: 'Rainfall',
-        fill: false,
-        lineTension: 0.5,
-        backgroundColor: 'rgba(75,192,192,1)',
-        borderColor: 'rgba(0,0,0,1)',
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56]
-      }
-    ]
-  }
+
   
 
   return (
     <div className="Stocks">
     <StockSearchBar></StockSearchBar>
   <p>{stock}{" $"}{price}</p>
-  <Line
-          data={chartdata}
-          options={{
-            title:{
-              display:true,
-              text:stock,
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
+  
   </div>
   );
 }
